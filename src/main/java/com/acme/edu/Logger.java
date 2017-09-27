b3ccfdb953412e73ee01df13af4093a401c2f834
AlexBaklaev <AlexBaklaev@gmail.com>
2017-09-27 20:20:30 +0300

    Course Project Iteration 01: The Rise of Logger Library

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,25 @@
 package com.acme.edu;
 
 public class Logger {
+    private static final String PRIMITIVE_PREFIX = "primitive: ";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        print(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        print(PRIMITIVE_PREFIX + message);
+    }
+
+    public static void log(char message) {
+        print("char: " + message);
+    }
+
+    public static void log(String message) {
+        print("string: " + message);
+    }
+
+    private static void print(String message) {
+        System.out.println(message);
     }
 }
